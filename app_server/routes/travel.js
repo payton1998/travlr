d893bbfbb3d0ee8205e36f4df01e9345638c0895
Payton Mitchell <payton016@gmail.com>
2022-04-04 10:19:22 -0400

    module5

--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller= require('../controllers/travel');
-/* GET home page. */
-router.get('/', controller.travel);
+const controller = require("../controllers/travel");
+
+router.get("/", controller.travelList);
+
 module.exports = router;
